c0f591d60f7c6ddb59b6a7c8364243e9d45cdb6e
snowlavenderlove <963902930@qq.com>
2019-05-30 17:22:58 +0800

    springboot

---
+++ src/main/java/com/xue/service/sys/UserService.java
@@ -0,0 +1,20 @@
+package com.xue.service.sys;
+
+import java.util.List;
+
+import com.xue.entity.model.User;
+
+public interface UserService {
+	
+	//增
+	public int addUser(User user);
+	//删
+	public int deleteUser(int id);
+	//改	
+	public int updateUser(User user);
+	//查	
+	public List<User> getUser();
+	
+	
+
+}
